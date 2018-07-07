e0b2d754ee8ed832e7955e31d9338c46dfe4a1d6
Ron-Zheng <Ron-Zheng@users.noreply.github.com>
2018-07-07 11:02:58 +0800

    Add files via upload

---
+++ micro-weather-basic/src/main/java/com/ron/weather/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.ron.weather.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/hello")
+	public String hello() {
+		return "Hello Word";
+	}
+}
---
+++ micro-weather-basic/src/main/java/com/ron/weather/service/WeatherDataService.java
@@ -0,0 +1,53 @@
+package com.ron.weather.service;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ron.weather.facade.WeatherDataFacade;
+import com.ron.weather.vo.WeatherResponse;
+
+@Service
+public class WeatherDataService implements WeatherDataFacade {
+	
+	private static final String WEATHER_URI = "http://wthrcdn.etouch.cn/weather_mini?";
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@Override
+	public WeatherResponse getDataByCityId(String cityId) {
+		String uri = WEATHER_URI + "citykey=" + cityId;
+		return this.doGetWeahter(uri);
+	}
+
+	@Override
+	public WeatherResponse getDataByCityName(String cityName) {
+		String uri = WEATHER_URI + "city=" + cityName;
+		return this.doGetWeahter(uri);
+	}
+	
+	private WeatherResponse doGetWeahter(String uri) {
+ 		ResponseEntity<String> respString = restTemplate.getForEntity(uri, String.class);
+		
+		ObjectMapper mapper = new ObjectMapper();
+		WeatherResponse resp = null;
+		String strBody = null;
+		
+		if (respString.getStatusCodeValue() == 200) {
+			strBody = respString.getBody();
+		}
+
+		try {
+			resp = mapper.readValue(strBody, WeatherResponse.class);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return resp;
+	}
+}
